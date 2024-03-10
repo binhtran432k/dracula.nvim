ab7d7bd763f58f8b86ba0268094b0ccd6c00cd35
Binh Duc Tran <binhtran432k@gmail.com>
2024-03-10 12:24:16 +0700

    fix(theme): apply VSCode style for DiagnosticUnnecessary

--- lua/dracula/theme.lua
+++ lua/dracula/theme.lua
@@ -188,7 +188,7 @@ function M.setup()
     DiagnosticWarn = { fg = c.warning }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default
     DiagnosticInfo = { fg = c.info }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default
     DiagnosticHint = { fg = c.hint }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default
-    DiagnosticUnnecessary = { fg = c.comment }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default
+    DiagnosticUnnecessary = { fg = make_alpha(c.fg, 0.6) }, -- Used as the base highlight group. Other Diagnostic highlights link to this by default
 
     DiagnosticVirtualTextError = { bg = make_alpha(c.error, 0.1), fg = c.error }, -- Used for "Error" diagnostic virtual text
     DiagnosticVirtualTextWarn = { bg = make_alpha(c.warning, 0.1), fg = c.warning }, -- Used for "Warning" diagnostic virtual text
